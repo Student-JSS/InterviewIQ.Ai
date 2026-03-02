250fd95aff85f23fc9d16858cf9c1c134d436806
shivam <shiv10212005@gmail.com>
2026-03-02 15:46:40 +0530

    setup for creating project

---
+++ server/config/connectDb.js
@@ -0,0 +1,11 @@
+import mongoose, { connect } from "mongoose";
+
+const connectDb = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error(`Database connection error: ${error}`);
+    }
+}
+export default connectDb;
